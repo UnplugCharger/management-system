ce8365c104eeb075eb7aeb218a7228efbf88e5b2
UnplugCharger <isaacbyron@gmail.com>
2021-11-14 15:19:44 +0300

    wip

--- models/orderItem.go
+++ models/orderItem.go
@@ -16,9 +16,9 @@ type OrderItem struct {
 	ID  primitive.ObjectID  `bson:"_id"`
 	Quantity   *string  `json:"quantity" validate:"required,eq=S|eq=M|eq=L"`
 	Unit_price *float64  `json:"unit_price" validate:"required"`
-	Created_at time.Time   
-	Updated_at time.Time 
-	Food_id  *string 
-	Order_item_id  string
-	Order_id  string
+	Created_at time.Time   `json:"created_at"`
+	Updated_at time.Time `json:"updated_at"`
+	Food_id  *string   `json:"food_id" validate:"required"`
+	Order_item_id  string `json:"order_item_id"`
+	Order_id  *string `json:"order_id" validation:"required"`
 }
--- models/userModel.go
+++ models/userModel.go
@@ -1,1 +1,24 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+
+type User struct {
+	ID primitive.ObjectID  `bson:"_id"`
+	First_name   *string 
+	Last_name  *string 
+	Password *string 
+	Email *string 
+	Avatar *string 
+	Phone *string 
+	Token *string 
+	Refresh_token *string 
+	Created_at time.Time
+	Updated_at time.Time
+	User_id  string
+} 

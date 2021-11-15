8f5a234941e8d3afc696a491eb14cdace552a90d
UnplugCharger <isaacbyron@gmail.com>
2021-11-15 11:13:24 +0300

    wip

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -2,6 +2,9 @@ package controllers
 
 import (
 	"context"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/byron/rest/database"
 	"github.com/gin-gonic/gin"
@@ -14,8 +17,17 @@ var menuCollection *mongo.Collection= database.OpenCollection(database.Client,"m
 
 func GetMenus() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx , cancel = context.WithTimeout(context.Background(),100*time.Second)
 		result, err := menuCollection.Find(context.TODO(),bson.M{})
 		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error ocurred while trying to list menus"})
+		}
+		var allMenus []bson.M
+		if err = result.All(ctx , &allMenus); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK,allMenus)
 	}
 }
 

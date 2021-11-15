44203d4eb893d37e9531cea2801dfc44ad4e7f6d
UnplugCharger <isaacbyron@gmail.com>
2021-11-15 11:41:16 +0300

    wip

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -2,13 +2,16 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/byron/rest/database"
+	"github.com/byron/rest/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -32,10 +35,55 @@ func GetMenus() gin.HandlerFunc {
 }
 
 func GetMenu() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	
+		return func(c *gin.Context) {
+			var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+			menuId := c.Param("menu_id")
+			var menu models.Menu
+	
+			err := foodCollection.FindOne(ctx, bson.M{"menu_id": menuId}).Decode(&menu)
+	
+			defer cancel()
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching  the menu "})
+			}
+			c.JSON(http.StatusOK, menu)
+		}
+	
 }
 func CreateMenu() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+        var menu  models.Menu
+
+
+		if err := c.BindJSON(&menu); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		validationErr := validate.Struct(menu)
+
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+
+		menu.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		menu.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		menu.ID = primitive.NewObjectID()
+		menu.Food_id = menu.ID.Hex()
+         
+
+		result , insertErr := menuCollection.InsertOne(ctx,menu)
+
+		if insertErr != nil {
+			msg := fmt.Sprintf("Menu Item was not Created")
+			c.JSON(http.StatusInternalServerError,gin.H{"error":msg})
+		}
+		defer cancel()
+		c.JSON(http.StatusOK,result)
+	}
 }
 
 func UpdateMenu() gin.HandlerFunc {
